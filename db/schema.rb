3e9a79a2985a87869a343daf98096dc5b8652960
Jeremy Ng Cheng Hin <jngchenghin@gmail.com>
2015-10-01 05:17:00 -0700

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001113010) do
+ActiveRecord::Schema.define(version: 20151001120138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "rectangles", force: :cascade do |t|
-    t.integer  "Width"
-    t.integer  "Height"
-    t.decimal  "Fill"
-    t.string   "FillColor"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "Color"
-  end
 
 end
